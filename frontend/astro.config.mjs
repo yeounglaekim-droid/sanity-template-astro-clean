c4dfaf6bd9280d3f050fb5067c7955fd8f3edc04
yeounglaekim-droid <yeounglaekim@gmail.com>
2026-09-15 14:32:55 +0900

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -15,31 +15,27 @@ const studioUrl = PUBLIC_SANITY_STUDIO_URL || "http://localhost:3333";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
-// Change this depending on your hosting provider (Vercel, Netlify etc)
-// https://docs.astro.build/en/guides/server-side-rendering/#adding-an-adapter
-import vercel from "@astrojs/vercel";
-
+// ⚠️ 클라우드플레어 어댑터로 변경!
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   // Set to 'server' for Visual Editing and on-demand rendering
-  // Requires an adapter for deployment (Vercel, Netlify, Cloudflare, Node, etc.)
   output: "server",
-  adapter: vercel(),
+  // ⚠️ Vercel을 지우고 Cloudflare를 장착합니다.
+  adapter: cloudflare(),
   integrations: [
     sanity({
       projectId,
       dataset,
-      // studioBasePath: "/admin",
-      // Set useCdn to false if you're building statically.
       useCdn: false,
-      apiVersion: "2026-03-26", // Set to date of setup to use the latest API version
+      apiVersion: "2026-03-26", 
       stega: {
         studioUrl,
       },
     }),
-    react(), // Required for Sanity Studio
+    react(), 
   ],
   vite: {
     optimizeDeps: {
@@ -52,7 +48,6 @@ export default defineConfig({
         "lodash/sortedIndex.js",
       ],
     },
-
     plugins: [tailwindcss()],
   },
 });
